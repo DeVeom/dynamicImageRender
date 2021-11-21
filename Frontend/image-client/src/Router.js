2052651d4714833733f6ec69fa8b8196c1b5a49d
이름 <ktg6361@gmail.com>
2021-11-22 00:04:29 +0900

    Add: svg로 분석리포트 구현중

--- Frontend/image-client/src/Router.js
+++ Frontend/image-client/src/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Report from './pages/Report/Report';
+import Svg from './pages/SVG/Svg';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/report" element={<Report />} />
+        <Route path="/svg" element={<Svg />} />
       </Routes>
     </BrowserRouter>
   );
