26782933640234aa734d2500dc43a1d5b474f92e
이름 <ktg6361@gmail.com>
2021-11-23 21:27:42 +0900

    Add: 프/백 통신 및 SVG 분석리포트 상세 페이지에 구현

--- Frontend/main-client/src/client.js
+++ Frontend/main-client/src/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://862b-116-124-50-130.ngrok.io/graphql',
+  uri: 'http://localhost:8080/graphql',
   cache: new InMemoryCache(),
 });
 
