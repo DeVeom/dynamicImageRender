cf92896ec1f332fa68273235cdaa35eecb17f0ba
이름 <ktg6361@gmail.com>
2021-11-24 14:32:28 +0900

    Add: 임베디드 코드 구현중

--- Frontend/main-client/src/pages/Detail/components/AnalysisMenu.js
+++ Frontend/main-client/src/pages/Detail/components/AnalysisMenu.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './AnalysisMenu.module.css';
 import { useMutation, gql } from '@apollo/client';
 
 const AnalysisMenu = ({ channelId }) => {
+  const [imgUrl, setImgUrl] = useState('');
+
   const SEND_DATA = gql`
   mutation {
     generateScreenshot(channelId: "${channelId}")
   }
 `;
 
-  const [mutateFunction] = useMutation(SEND_DATA);
+  const [mutateFunction, { loading, error, data }] = useMutation(SEND_DATA);
+
+  const handleMutation = async () => {
+    const result = await mutateFunction();
+    console.log(result);
+  };
 
   return (
     <div className={style.analysisMenu}>
@@ -20,7 +27,7 @@ const AnalysisMenu = ({ channelId }) => {
         <p className={style.analysisMenuText}>광고 단가</p>
         <p className={style.analysisMenuText}>관련 채널 분석</p>
       </div>
-      <div className={style.analysisMenuText} onClick={mutateFunction}>
+      <div className={style.analysisMenuText} onClick={handleMutation}>
         임베디드 코드 버튼
       </div>
     </div>
